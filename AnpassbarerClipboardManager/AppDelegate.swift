828828c9e7a071c23313d4216f9947d876bad150
Philipp Waldhauer <ich@philippwaldhauer.de>
2019-02-14 16:10:02 +0100

    Initial Commit

---
+++ AnpassbarerClipboardManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnpassbarerClipboardManager
+//
+//  Created by Philipp Waldhauer on 14.02.19.
+//  Copyright © 2019 io.pwa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AnpassbarerClipboardManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AnpassbarerClipboardManager
+//
+//  Created by Philipp Waldhauer on 14.02.19.
+//  Copyright © 2019 io.pwa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
